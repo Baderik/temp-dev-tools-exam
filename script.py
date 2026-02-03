85a9913e2c10a0a071e3396d2f756098c8ec9bc2
Baderik <baderik.pm@gmail.com>
2026-02-03 14:58:36 +0300

    feat(script): add script

---
+++ script.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+if name == "main":
+    today = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    
+    print(f"Дата и время: {today}")
+    print("Бадерик Павел Максимович")
+    print("Ухачина Амина Михайловна")
